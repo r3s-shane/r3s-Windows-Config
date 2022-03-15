e0d82d28cc8363faab8da44fcb5c1718b3833785
Shane <30303397+shanepeden@users.noreply.github.com>
2022-03-15 09:54:26 -0400

    Update OneVizion-Windows-Config.ps1

--- OneVizion-Windows-Config.ps1
+++ OneVizion-Windows-Config.ps1
@@ -1,8 +1,16 @@
 $text = @"
 OneVizion's enduser laptop configuration script
+
+To run this script:
+1. Save the file to C:\ or another directory you can easily navigate to in Powershell. Make sure you change the file type from a .txt to a .ps1 (change the folder options to display file extensions for known file types)
+2. Open Powershell as an admin and navigate to the directory where you stored this OutFile (e.g., CD c:\)
+3. Change the remote execution policy by typing:  set-executionpolicy remotesigned   Then type "Y"
+4. Run this Powershell script by typing its name and hitting enter
+5. Change the remote execution policy back by typing: set-executionpolicy remotesigned   Then type "N"
+6. IM Shane if you get stuck or something breaks.
 "@
 
-#download the installation assets from our S3 bucket
+#download the installation assets
 New-Item -Path "c:\" -Name "new_install" -ItemType "directory"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/shanepeden/OneVizion-Windows-Config/master/standard_packages.txt -OutFile C:\new_install\standard_packages.txt
 
@@ -25,9 +33,7 @@ BackupToAAD-BitLockerKeyProtector $env:systemdrive -KeyProtectorId $RecoveryProt
 Get-AppxPackage Microsoft.windowscommunicationsapps | Remove-AppxPackage
 
 # install chocolatey if not installed
-if (!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
-  Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-}
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # for each package in the list run install
 Get-Content "c:\new_install\standard_packages.txt" | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install -y $_}
